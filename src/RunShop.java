b874420c3bb76555061e9f859191f26ca0b882c2
riddle-master-ruben <119084862+riddle-master-ruben@users.noreply.github.com>
2024-03-29 16:04:16 -0600

    Potential File Scanner

--- src/RunShop.java
+++ src/RunShop.java
@@ -1,9 +1,16 @@
+import vehicles.*;
+import entity.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * This is the main runner class.
  * @author Ashkan Arabi
  * @author James Newson
  * @author Ruben Martinez
- * @version Part 1
+ * @version Part One
  */
 
 public class RunShop {
@@ -13,6 +20,23 @@ public class RunShop {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        scanCarData();
+    }
+    
+    /**
+     * Method that scans the car data csv file and initalizes objects.
+     * @return Scanner reference object whose cursor is at the beginning of the file.
+     */
+    public static Scanner scanCarData() {
+        Scanner carDataScanner = null;
+        try {
+            carDataScanner = new Scanner(new File("data/car_data.csv"));
+            System.out.println(carDataScanner.nextLine());
+
+        }
+        catch (FileNotFoundException f) {
+            System.out.println("File not found.");
+        }
+        return carDataScanner;
     }
 }
