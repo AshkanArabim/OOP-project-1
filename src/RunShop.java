d395b0665aae6265b29c95596952581ca4365469
riddle-master-ruben <119084862+riddle-master-ruben@users.noreply.github.com>
2024-04-06 10:51:36 -0600

    Academic Integrity Statement

--- src/RunShop.java
+++ src/RunShop.java
@@ -1,3 +1,31 @@
+/*
+ * Academic Integrity Statement
+ * This work is to be done as a team. It is not permitted to share, reproduce, or alter
+ * any part of this assignment for any purpose. Students are not permitted to share
+ * code, upload this assignment online in any form, or view/receive/modify code
+ * written from anyone else. This assignment is part of an academic course at The
+ * University of Texas at El Paso and a grade will be assigned for the work produced
+ * individually by the student.
+ * 
+ * Names of individual(s) who contributed to this work:
+ * Ashkan Arabi
+ * James Newson
+ * Ruben Martinez
+ * 
+ * Date: April 6th, 2024
+ * Course: CS 3331 Advanced Object Oriented Programming
+ * Instructor: Bhanukiran Gurijala
+ * Programming Assignment : 1
+ * 
+ * Description: The system offers both brand new and used cars for a car dealership Mine Cars. It provides
+ * a wide variety of different models with varying prices and mileage to satisfy customers. Customers have budgets
+ * and our system maintains and tracks a portfolio for each customer via a CSV file. In addition, customers
+ * can sort through our vehicles, and admins can view tickets for every car purchased from our dealership. Finally,
+ * memebrs can opt for a membership to recieve discounts or better interest rates.
+ * 
+ */
+
+
 // import statements
 /************************************************************************/
 import java.util.Scanner;
@@ -14,7 +42,7 @@ import UI.*;
 
 
 /**
- * This is the main runner class.
+ * This is the main runner class that is responsible for signing in users and admins, and also selling cars.
  * @author Ashkan Arabi
  * @author James Newson
  * @author Ruben Martinez
